d8a9189ace521ced7949385ccae0ced115f571d0
omkar8844 <66157095+omkar8844@users.noreply.github.com>
2025-12-05 12:42:42 +0530

    Refactor MongoDB connections and data retrieval
    Refactor MongoDB connections and streamline data retrieval for store insights and wallet information.

--- Streamlit_Mongo_Test.py
+++ Streamlit_Mongo_Test.py
@@ -4,7 +4,8 @@ import pandas as pd
 import datetime
 from datetime import datetime
 import altair as alt
-# Custom HTML/CSS for KPI cards
+
+#HTML BLOCKS
 def styled_metric(label, value, bg_color="#2E86C1", font_color="#FFFFFF", label_size="20px", value_size="32px"):
     st.markdown(
         f"""
@@ -24,36 +25,47 @@ def styled_metric(label, value, bg_color="#2E86C1", font_color="#FFFFFF", label_
         """,
         unsafe_allow_html=True
     )
-
+    
+#Mongo Connection
 mongo_uri = st.secrets.get("mongodb", {}).get("uri") if st.secrets else None
-
+#mongo_uri = st.secrets["mongo"]["uri"]
 client=MongoClient(mongo_uri)
+#Database Connectons
 db_retail=client['apeirosretail']
-storedetails_collection=db_retail['storeDetails']
 db_bills=client['apeirosretaildataprocessing']
+db_wallet=client['apeirosretailcustomermanagement']
+#Collection Connections
+#1
+storedetails_collection=db_retail['storeDetails']
+org=db_retail['organizationDetails']
+#2
 billReq=db_bills['billRequest']
-store_names=storedetails_collection.distinct("storeName")
-ind=store_names.index('SAARA')
+in_ex=db_bills['invoiceExtractedData']
+rec_ex=db_bills['receiptExtractedData']
+trans_bill=db_bills['billtransactions']
+#3
+wallet_collection=db_wallet['promotionalMessageCredit']
+
+#Visual Daily bill count bar graph
 st.title("Today's Bill Count")
 today = datetime.today()
 start = datetime(today.year, today.month, today.day)
 end = datetime(today.year, today.month, today.day, 23, 59, 59)
+
 bill_docs_bar = list(billReq.find({"createdAt": {"$gte": start, "$lte": end}},{"billId": 1, "storeId": 1, "_id": 0}))
 if bill_docs_bar:
     today_bill_df=(pd.DataFrame(bill_docs_bar))
     store_ids_bar = today_bill_df["storeId"].unique().tolist()
     store_map=[]
     #({'billId': {'$in': bill_ids}}))
-    for i in list(storedetails_collection.find({'_id':{'$in':store_ids_bar}})):
+    for i in list(storedetails_collection.find({'_id':{'$in':store_ids_bar}},{"_id":1,"storeName":1})):
         store_map.append({
             "storeId":i['_id'],
             "storeName":i['storeName']
             })
-    #store_map = {doc["_id"]: doc["storeName"] for doc in storedetails_collection.find()}
     store_map_df=(pd.DataFrame(store_map))
     today_bill_df=today_bill_df.merge(store_map_df,on='storeId',how='inner')
     st.write("Today's Bill Count- ",today_bill_df['billId'].nunique())
-
     bill_count_df = (
         today_bill_df.groupby("storeName")["billId"]
         .count()
@@ -74,40 +86,43 @@ if bill_docs_bar:
     st.altair_chart(chart, use_container_width=True)
 else:
     st.subheader('No Bills Till Now')
-#------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------------
+#Store insights
 st.title("Store Insights 📺")
+store_names=storedetails_collection.distinct("storeName")
+ind=store_names.index('SAARA')
+#Select store widget
 selected_store=st.selectbox("Choose a store",store_names,index=ind)
 if selected_store:
-    #Org Processing
-    org=db_retail['organizationDetails']
-    #Bill Request Processing
-
-    #Getting selected stores data
-    docs = list(storedetails_collection.find({"storeName": selected_store}))
-    #Getting storeId
-    for doc in docs:
+    store_doc=list(storedetails_collection.find({"storeName": selected_store},{'_id':1,'storeName':1,'tenantId':1,'createdAt':1}))
+    for doc in store_doc:
         storeId=doc['_id']
     #Getting tenantId
-    for doc2 in docs:
+    for doc2 in store_doc:
         tenantId=doc2['tenantId']
     #Getting onboard date
-    for doc3 in docs:
+    for doc3 in store_doc:
         createdAt=doc3['createdAt']
     onboard_date=createdAt.strftime('%d %B %Y') 
+    #Getting respective org
+    org_doc=list(org.find({'tenantId':tenantId},{'tenantId':1,'phoneNumber':1}))
+    #Getting Phone number
+    phone_list=[i['phoneNumber'] for i in org_doc]
+    phone_value=phone_list[0][0]
     
-    #Getting respective org    
-    org_doc=list(org.find({'tenantId':tenantId}))
-    
-    #Getting respective bill
-    bill_doc=list(billReq.find({'storeId':storeId}))
-    
-    #Getting respective bill invoice extracted data
-    bill_ids=[i['billId'] for i in bill_doc]
-    in_ex=db_bills['invoiceExtractedData']
-    in_ex_docs = list(in_ex.find({'billId': {'$in': bill_ids}}))
+    #for Bill amount sum
     total_in_amount=0
     total_rec_amount=0
     total_trans_amount=0
+    #getting respective bills
+    bill_doc=list(billReq.find({'storeId':storeId},{'storeId':1,'billId':1,'createdAt':1,'name':1}))
+   
+    bill_ids=[i['billId'] for i in bill_doc]    
+    #Getting bill ID count
+    bill_count=len(set(bill_ids))
+    #Getting respective bill invoice extracted data
+    in_ex_docs = list(in_ex.find({'billId': {'$in': bill_ids}},{"billId":1,"InvoiceTotal":1,"_id":0}))
     if in_ex_docs:
         total_in_amount = sum(
             float(i['InvoiceTotal']['value'])
@@ -117,34 +132,29 @@ if selected_store:
             and i['InvoiceTotal']['value'] is not None
             and i['InvoiceTotal']['value'] != ""
         )
-     
-#Getting respective bills from rec extracted
-    rec_ex=db_bills['receiptExtractedData'] 
-    rec_ex_docs=list(rec_ex.find({'billId':{'$in':bill_ids}})) 
+        
+    #Getting respective bills from rec extracted
+    rec_ex_docs=list(rec_ex.find({'billId':{'$in':bill_ids}},{'billId':1,'Total':1,'_id':0}))
     if rec_ex_docs:
         total_rec_amount=sum(
             float(i['Total']['value'])for i in rec_ex_docs
                   if i['Total']['value'] is not None
                   and i['Total']['value']!="")
-        
-        
-#Getting respective bills from bill_trans
-    trans_bill=db_bills['billtransactions']
-    trans_bill_docs=list(trans_bill.find({'billId':{'$in':bill_ids}}))
+    #Getting respective bills from bill_trans
+    trans_bill_docs=list(trans_bill.find({'billId':{'$in':bill_ids}},{'billId':1,'billAmount':1}))
     if trans_bill_docs:
         total_trans_amount=sum(
             float(i['billAmount']) for i in trans_bill_docs
             if i['billAmount'] is not None
             and i['billAmount']!=""
         )
-        
+    
     #Getting total rev
     final_total_rev=int(total_rec_amount+total_trans_amount+total_in_amount)
     
     #Getting Wallet Data
-    db_wallet=client['apeirosretailcustomermanagement']
     wallet_collection=db_wallet['promotionalMessageCredit']
-    wallet_doc=list(wallet_collection.find({'tenantId':tenantId}))
+    wallet_doc=list(wallet_collection.find({'tenantId':tenantId},{'tenantId':1,'currentAvailable':1,'lifetimeConsumption':1,'_id':0}))  
     wallet_bal=[i['currentAvailable'] for i in wallet_doc]
     wallet_cons=[i['lifetimeConsumption'] for i in wallet_doc]
     if wallet_bal:
@@ -155,21 +165,11 @@ if selected_store:
         wallet_consuption=round(wallet_cons[0],2)
     else:
         wallet_consuption=0
-
-    #Getting bill ID count
-    bill_count=len(set(bill_ids))
-    
-    #Getting Phone number
-    phone_list=[i['phoneNumber'] for i in org_doc]
-    phone_value=phone_list[0][0]
-
     #Plotting the results
     a,b=st.columns(2,gap="small")
     with a:
-        st.space(size="small") 
         styled_metric("Phone Number 📞", phone_value, bg_color="#34495E", font_color="#F1C40F", label_size="20px", value_size="28px")
     with b:
-        st.space(size="small") 
         styled_metric("Onboard Date ✈️", onboard_date, bg_color="#34495E", font_color="#F1C40F", label_size="20px", value_size="28px")
     c,d=st.columns(2,gap="small")
     with c:
@@ -182,10 +182,8 @@ if selected_store:
     st.subheader("Wallet Information")   
     e,f=st.columns(2,gap='small')
     with e:
-        st.space(size="small") 
         styled_metric("Wallet Balance 💼", wallet_balance, bg_color="#34495E", font_color="#F1C40F", label_size="20px", value_size="28px")
     with f:
-        st.space(size="small") 
         styled_metric("Wallet Consumption ⚡", wallet_consuption, bg_color="#34495E", font_color="#F1C40F", label_size="20px", value_size="28px")
         
         
@@ -193,50 +191,14 @@ if selected_store:
     show_bills = st.checkbox("Show Bills") 
     if show_bills: 
         st.dataframe(bill_doc)
-
-
-
-#---------------------------------------------        
-#preparing for bar graph
-# today = datetime.today()
-# start = datetime(today.year, today.month, today.day)
-# end = datetime(today.year, today.month, today.day, 23, 59, 59)
-# bill_docs_bar = list(billReq.find({"createdAt": {"$gte": start, "$lte": end}},{"billId": 1, "storeId": 1, "_id": 0}))
-# today_bill_df=(pd.DataFrame(bill_docs_bar))
-# store_ids_bar = today_bill_df["storeId"].unique().tolist()
-# store_map=[]
-# #({'billId': {'$in': bill_ids}}))
-# for i in list(storedetails_collection.find({'_id':{'$in':store_ids_bar}})):
-#     store_map.append({
-#         "storeId":i['_id'],
-#         "storeName":i['storeName']
-#         })
-# #store_map = {doc["_id"]: doc["storeName"] for doc in storedetails_collection.find()}
-# store_map_df=(pd.DataFrame(store_map))
-# today_bill_df=today_bill_df.merge(store_map_df,on='storeId',how='inner')
-
-# bill_count_df = (
-#     today_bill_df.groupby("storeName")["billId"]
-#     .count()
-#     .reset_index()
-#     .rename(columns={"billId": "billCount"})
-# )
-# # st.bar_chart(
-#     data=bill_count_df,
-#     x="storeName",
-#     y="billCount",
-# )
-# import altair as alt
-
-# chart = (
-#     alt.Chart(bill_count_df)
-#     .mark_bar()
-#     .encode(
-#         x=alt.X("storeName:N", sort="-y", title="Store Name"),
-#         y=alt.Y("billCount:Q", title="Number of Bills"),
-#         tooltip=["storeName", "billCount"]
-#     )
-#     .properties(height=400)
-# )
-
-# st.altair_chart(chart, use_container_width=True)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
